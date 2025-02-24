148ad6d820ef43b5a2bb49a1828cb95904b7be09
elverdongrowshop <juarez.silvio@gmail.com>
2025-02-24 13:41:27 -0300

    Agregar carpeta pages con index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="p-6 text-center">
+      <h1>Bienvenido a El Verdon Growshop</h1>
+      <p>Todo lo necesario para el cultivo a tu alcance.</p>
+    </div>
+  );
